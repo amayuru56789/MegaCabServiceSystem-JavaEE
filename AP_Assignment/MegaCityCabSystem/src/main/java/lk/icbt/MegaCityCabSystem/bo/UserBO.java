83795f2c91f15f81dc896a5cd00e65cfe4c4cfef
Amayuru Indeewara <amayuru_i@epic.lk>
2025-02-04 19:31:28 +0530

    getting interface for loose couple

---
+++ AP_Assignment/MegaCityCabSystem/src/main/java/lk/icbt/MegaCityCabSystem/bo/UserBO.java
@@ -0,0 +1,5 @@
+package lk.icbt.MegaCityCabSystem.bo;
+
+public interface UserBO {
+    boolean equalityUser(String user, String password);
+}
---
+++ AP_Assignment/MegaCityCabSystem/src/main/java/lk/icbt/MegaCityCabSystem/bo/impl/UserBOImpl.java
@@ -0,0 +1,10 @@
+package lk.icbt.MegaCityCabSystem.bo.impl;
+
+import lk.icbt.MegaCityCabSystem.bo.UserBO;
+
+public class UserBOImpl implements UserBO {
+    @Override
+    public boolean equalityUser(String user, String password) {
+        return false;
+    }
+}
